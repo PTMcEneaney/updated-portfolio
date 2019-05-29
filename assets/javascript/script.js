3f9caf2355b410b4c5c41df6096fcbfedc499bee
PTMcEneaney <pagemceneaney@gmail.com>
2019-05-28 17:06:37 -0700

    added audio functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,14 @@
-$( document ).ready(function) {
-    $(".dropdown-button").dropdown();
-}
+$(document).ready(function() {
+
+    // var sound = new Audio("../images/AudioMcEneaney.WAV");
+    //     // $('.volume').click(e => sound.play());
+    // $('document').on("click", ".volume", function(){
+    //     console.log("clicked");
+    // });
+    // $(".mceneaney").on("click", function(){
+    //     console.log("clicked");
+
+    // });
+
+
+});
